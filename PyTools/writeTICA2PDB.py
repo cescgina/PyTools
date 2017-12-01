2d3644449e7d4bb3ae85905c31e6296c26ec5e00
jgilaber <joan.gilabert@bsc.es>
2017-12-01 12:59:41 +0100

    Add cli and stride argument to writeTICA2PDB

--- PyTools/writeTICA2PDB.py
+++ PyTools/writeTICA2PDB.py
@@ -1,4 +1,5 @@
 import glob
+import argparse
 import numpy as np
 from AdaptivePELE.utilities import utilities
 
@@ -8,24 +9,36 @@ def sort_split_by_numbers(traj_name):
     return (int(trajNum), int(snapNum[:-4]))
 
 
-data_folder = "tica_COM/"
-COM_tica = []
-files = glob.glob(data_folder+"traj_*")
-files.sort(key=sort_split_by_numbers)
-for traj in files:
-    traj_data = np.loadtxt(traj)
-    if len(traj_data.shape) < 2:
-        COM_tica.append(traj_data.tolist())
-    else:
-        COM_tica.extend(traj_data.tolist())
+def parse_arguments():
+    """
+        Create command-line interface
+    """
+    desc = "Write the time-structure based Independent Components (TICA) from a simulation into a pdb file"
+    parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument("-stride", type=int, default=1, help="Stride, e.g. select one conformation out of every x, default 1, that is take all")
+    args = parser.parse_args()
+    return args.stride
 
-COM_tica = np.array(COM_tica)
-# process each TICA so the beta value visualization works (shift to 0)
-COM_tica[:, 3:] -= np.min(COM_tica[:, 3:], axis=0)
+if __name__ == "__main__":
+    stride = parse_arguments()
+    data_folder = "tica_COM/"
+    COM_tica = []
+    files = glob.glob(data_folder+"traj_*")
+    files.sort(key=sort_split_by_numbers)
+    for traj in files:
+        traj_data = np.loadtxt(traj)
+        if len(traj_data.shape) < 2:
+            COM_tica.append(traj_data.tolist())
+        else:
+            COM_tica.extend(traj_data[::stride].tolist())
 
-utilities.makeFolder("tica_pdb")
-nConf, nTICS = COM_tica.shape
-ind = [0, 1, 2, 0]
-for i in xrange(3, nTICS):
-    ind[-1] = i
-    utilities.write_PDB_clusters(COM_tica[:, ind], title="tica_pdb/tica_%d.pdb" % (i-2), use_beta=True)
+    COM_tica = np.array(COM_tica)
+    # process each TICA so the beta value visualization works (shift to 0)
+    COM_tica[:, 3:] -= np.min(COM_tica[:, 3:], axis=0)
+
+    utilities.makeFolder("tica_pdb")
+    nConf, nTICS = COM_tica.shape
+    ind = [0, 1, 2, 0]
+    for i in xrange(3, nTICS):
+        ind[-1] = i
+        utilities.write_PDB_clusters(COM_tica[:, ind], title="tica_pdb/tica_%d.pdb" % (i-2), use_beta=True)
