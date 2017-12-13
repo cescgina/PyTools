1ce8e7294b13af78f34bc1724e7d5d7d19e931ed
jgilaber <joan.gilabert@bsc.es>
2017-12-13 17:27:58 +0100

    Refactor and added coordinate extraction code in KMeans_allSnapshots

--- PyTools/KMeans_allSnapshots.py
+++ PyTools/KMeans_allSnapshots.py
@@ -3,15 +3,17 @@ import glob
 import numpy as np
 import argparse
 from AdaptivePELE.utilities import utilities
-from AdaptivePELE.testing import cluster
+from AdaptivePELE.testing import cluster, extractCoords
 
 
 def parseArgs():
     parser = argparse.ArgumentParser(description="Script that reclusters the Adaptive clusters")
     parser.add_argument('nClusters', type=int)
+    parser.add_argument("ligand_resname", type=str, help="Name of the ligand in the PDB")
+    parser.add_argument("-atomId", nargs="*", default="", help="Atoms to use for the coordinates of the conformation, if not specified use the center of mass")
     parser.add_argument('-o', type=str, help="Output folder")
     args = parser.parse_args()
-    return args.nClusters, args.o
+    return args.nClusters, args.ligand_resname, args.atomId, args.o
 
 
 def writePDB(pmf_xyzg, title="clusters.pdb"):
@@ -32,56 +34,63 @@ def writePDB(pmf_xyzg, title="clusters.pdb"):
 
 
 def writeInitialStructures(centers_info, filename_template):
-    for cluster_num in centersInfo:
-        epoch_num, traj_num, snap_num = map(int, centersInfo[cluster_num]['structure'])
+    for cluster_num in centers_info:
+        epoch_num, traj_num, snap_num = map(int, centers_info[cluster_num]['structure'])
         trajectory = "%d/trajectory_%d.pdb" % (epoch_num, traj_num)
         snapshots = utilities.getSnapshots(trajectory)
         with open(filename_template % cluster_num, "w") as fw:
             fw.write(snapshots[snap_num])
 
-n_clusters, output = parseArgs()
-trajectoryFolder = "allTrajs"
-trajectoryBasename = "traj*"
-stride = 1
-clusterCountsThreshold = 0
 
-folders = utilities.get_epoch_folders(".")
-folders.sort(key=int)
+def get_centers_info(trajectoryFolder, trajectoryBasename, num_clusters, clusterCenters):
+    centersInfo = {x: {"structure": None, "minDist": 1e6, "center": None} for x in xrange(num_clusters)}
+
+    trajFiles = glob.glob(os.path.join(trajectoryFolder, trajectoryBasename))
+    for traj in trajFiles:
+        _, epoch, iTraj = os.path.splitext(traj)[0].split("_", 3)
+        trajCoords = np.loadtxt(traj)
+        if len(trajCoords.shape) < 2:
+            trajCoords = [trajCoords]
+        for snapshot in trajCoords:
+            nSnap = snapshot[0]
+            snapshotCoords = snapshot[1:]
+            dist = np.sqrt(np.sum((clusterCenters-snapshotCoords)**2, axis=1))
+            for clusterInd in xrange(num_clusters):
+                if dist[clusterInd] < centersInfo[clusterInd]['minDist']:
+                    centersInfo[clusterInd]['minDist'] = dist[clusterInd]
+                    centersInfo[clusterInd]['structure'] = (epoch, int(iTraj), nSnap)
+                    centersInfo[clusterInd]['center'] = snapshotCoords
+    return centersInfo
 
 
-clusteringObject = cluster.Cluster(n_clusters, trajectoryFolder,
-                                   trajectoryBasename, alwaysCluster=False,
-                                   stride=stride)
-clusteringObject.clusterTrajectories()
-clusteringObject.eliminateLowPopulatedClusters(clusterCountsThreshold)
-clusterCenters = clusteringObject.clusterCenters
-dtrajs = clusteringObject.dtrajs
+def main(num_clusters, output_folder, ligand_resname, atom_ids):
+    extractCoords.main(folder_name=".", lig_resname=ligand_resname, non_Repeat=True, atom_Ids=atom_ids)
+    trajectoryFolder = "allTrajs"
+    trajectoryBasename = "traj*"
+    stride = 1
+    clusterCountsThreshold = 0
 
-centersInfo = {x: {"structure": None, "minDist": 1e6, "center": None} for x in xrange(n_clusters)}
+    folders = utilities.get_epoch_folders(".")
+    folders.sort(key=int)
 
-trajFiles = glob.glob(os.path.join(trajectoryFolder, trajectoryBasename))
-for traj in trajFiles:
-    _, epoch, iTraj = os.path.splitext(traj)[0].split("_", 3)
-    trajCoords = np.loadtxt(traj)
-    if len(trajCoords.shape) < 2:
-        trajCoords = [trajCoords]
-    for snapshot in trajCoords:
-        nSnap = snapshot[0]
-        snapshotCoords = snapshot[1:]
-        dist = np.sqrt(np.sum((clusterCenters-snapshotCoords)**2, axis=1))
-        for clusterInd in xrange(n_clusters):
-            if dist[clusterInd] < centersInfo[clusterInd]['minDist']:
-                centersInfo[clusterInd]['minDist'] = dist[clusterInd]
-                centersInfo[clusterInd]['structure'] = (epoch, int(iTraj), nSnap)
-                centersInfo[clusterInd]['center'] = snapshotCoords
+    clusteringObject = cluster.Cluster(num_clusters, trajectoryFolder,
+                                       trajectoryBasename, alwaysCluster=False,
+                                       stride=stride)
+    clusteringObject.clusterTrajectories()
+    clusteringObject.eliminateLowPopulatedClusters(clusterCountsThreshold)
+    clusterCenters = clusteringObject.clusterCenters
 
-COMArray = [centersInfo[i]['center'] for i in xrange(n_clusters)]
-IndexesSet = set([centersInfo[i]['structure'] for i in xrange(n_clusters)])
-if output is not None:
-    outputFolder = os.path.join(output, "")
-    if not os.path.exists(outputFolder):
-        os.makedirs(outputFolder)
-else:
-    outputFolder = ""
-writePDB(COMArray, outputFolder+"clusters_%d_KMeans_allSnapshots.pdb" % n_clusters)
-writeInitialStructures(centersInfo, outputFolder+"initial_%d.pdb")
+    centersInfo = get_centers_info(trajectoryFolder, trajectoryBasename, num_clusters, clusterCenters)
+    COMArray = [centersInfo[i]['center'] for i in xrange(num_clusters)]
+    if output_folder is not None:
+        outputFolder = os.path.join(output_folder, "")
+        if not os.path.exists(outputFolder):
+            os.makedirs(outputFolder)
+    else:
+        outputFolder = ""
+    writePDB(COMArray, outputFolder+"clusters_%d_KMeans_allSnapshots.pdb" % num_clusters)
+    writeInitialStructures(centersInfo, outputFolder+"initial_%d.pdb")
+
+if __name__ == "__main__":
+    n_clusters, lig_name, atom_id, output = parseArgs()
+    main(n_clusters, output, lig_name, atom_id)
